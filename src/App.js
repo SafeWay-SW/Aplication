c896b991e8810f104857205e56c0af0cbdcee96a
Dani Nazare <dany.nt.14@gmail.com>
2020-05-31 17:02:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {logoImg,
+        apresentacaoText,
+        loginButton,
+        linkhref} from './StyledMain';
+
+function App() {
+  return (
+      <>
+        <MeuBotao> Meu Botão</MeuBotao>
+      </>  
+  
+  );
+}
+
+export default App;
